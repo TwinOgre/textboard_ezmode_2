dc33aa07e7fbb6d5778b296bd4ec75c694e77bcb
TwinOgre <glad2you@naver.com>
2023-12-11 14:16:13 +0900

    phase1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("== 시스템 시작==");
+
+
+        while(true){
+            System.out.printf("명령) ");
+            String command = sc.nextLine().trim();
+            if(command.equals("종료")){
+                break;
+            }
+        }
+
+
+        sc.close();
     }
 }
