dc1f161fad9d45085fdc813fc6d46fecab26d73d
TwinOgre <glad2you@naver.com>
2023-12-11 14:31:11 +0900

    phase3.목록

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class Article {
     int id;
     String title;
